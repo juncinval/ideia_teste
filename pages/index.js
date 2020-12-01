42d3219d5c84be7b89d0387932c6be6fb4780d56
Ivamar Junior <hardjunior1@gmail.com>
2020-12-01 22:40:57 +0000

    optimizando página node com vecel

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,15 @@
-import {useState} from "react";
+import Link from "next/link";
+// import {useState} from "react";
 
 function Home(){
 		return (
 			<div>
 				<h1>Home</h1>
-				<Contador />
+				<Link href="/sobre">
+					<a >Acessar pagina sobre</a>
+				</Link>
 			</div>
 		)
 }
 
-function Contador(){
-	const [contador, setContador] = useState(1);
-	
-	function adiconaContador(){
-		setContador(contador + 1);
-	}
-	return (
-		<div>
-			<div>{contador}</div>
-			<button onClick={adiconaContador}>Adiciona</button>
-		</div>
-	)
-}
 export default Home
---
+++ pages/sobre.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+// import {useState} from "react";
+
+function sobre() {
+    return (
+        <div>
+            <h1>Sobre</h1>
+            <Link href="/">
+                <a >Acessar pagina home</a>
+            </Link>
+        </div>
+    )
+}
+
+export default sobre
